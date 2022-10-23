431fb634821518b4b1edafaf2ff6240f41afbb2e
pablo <pablocuri2015@hotmail.com>
2022-10-22 20:19:06 -0500

    deploy kubernetes

--- notification/src/main/java/com/quesito/notification/NotificationApplication.java
+++ notification/src/main/java/com/quesito/notification/NotificationApplication.java
@@ -7,12 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {"com.quesito.notification",
                             "com.quesito.amqp" }
 )
 @EnableEurekaClient
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
 
     public static void main(String[] args) {
--- notification/src/main/java/com/quesito/notification/NotificationConfig.java
+++ notification/src/main/java/com/quesito/notification/NotificationConfig.java
@@ -20,21 +20,21 @@ public class NotificationConfig {
     @Value("${rabbitmq.routing-keys.internal-notification}")
     private String internalNotificationRoutingKey;
 
-//    @Bean
-//    public TopicExchange internalTopicExchange(){
-//        return new TopicExchange(this.internalExchange);
-//    }
-//
-//    @Bean
-//    public Queue notificationQueue(){
-//        return new Queue(this.notificationQueue);
-//    }
-//    @Bean
-//    public Binding internalToNotificationBinding(){
-//        return BindingBuilder.bind(notificationQueue())
-//                .to(internalTopicExchange())
-//                .with(internalNotificationRoutingKey);
-//    }
+    @Bean
+    public TopicExchange internalTopicExchange(){
+        return new TopicExchange(this.internalExchange);
+    }
+
+    @Bean
+    public Queue notificationQueue(){
+        return new Queue(this.notificationQueue);
+    }
+    @Bean
+    public Binding internalToNotificationBinding(){
+        return BindingBuilder.bind(notificationQueue())
+                .to(internalTopicExchange())
+                .with(internalNotificationRoutingKey);
+    }
 
 
 }
